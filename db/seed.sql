f744825584efd701c2e238d5b68cebd720d9c73d
thevcr <thevcr@gmail.com>
2022-05-15 22:36:52 -0400

    initial files and db schema/seed

---
+++ db/seed.sql
@@ -0,0 +1,32 @@
+INSERT INTO department (name)
+VALUES
+  ('Sales'),
+  ('Client Services'),
+  ('Engineering'),
+  ('Product'),
+  ('Accounting'),
+  ('Account Management');
+
+INSERT INTO role (title, salary, department_id)
+VALUES
+  ('Salesperson', 40000, 1),
+  ('Software Engineer',  120000, 3),
+  ('Accountant',  75000, 5),
+  ('Product Manager', 100000, 4),
+  ('Client Services Specialist', 45000, 2),
+  ('Lead Engineer', 150000, 3),
+  ('Account Manager', 75000, 6);
+
+
+  INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES
+  ('James', 'Fraser', 1),
+  ('Jack', 'London', 1),
+  ('Robert', 'Bruce', 3),
+  ('Peter', 'Greenaway', 5),
+  ('Derek', 'Jarman',5),
+  ('Paolo', 'Pasolini', 2),
+  ('Aubrey', 'Beardsley', 3),
+  ('Tulse', 'Luper', 6),
+  ('William', 'Morris',4),
+  ('George', 'Shaw', 7);
